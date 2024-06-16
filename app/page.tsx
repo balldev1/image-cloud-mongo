57922d3d67582910adea384e95ae24110ab89efc
balldev1 <balldev1@hotmail.com>
2024-06-16 16:44:46 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,138 @@
-import Image from "next/image";
+"use client"
+import {useCallback, useState} from 'react';
+import axios from "axios";
+import { CldUploadWidget } from 'next-cloudinary';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [department, setDepartment] = useState('');
+    const [role, setRole] = useState('');
+    const [img, setImg] = useState('');
+    const [uploadedImageUrl, setUploadedImageUrl] = useState('');
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    // Update img state with secure URL
+    const handleUpload = useCallback((result: any) => {
+        setImg(result.info.secure_url);
+        setUploadedImageUrl(result.info.secure_url); // Set the uploaded image URL for display
+    }, [setImg, setUploadedImageUrl]);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const handleSubmit = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                username,
+                password,
+                firstname,
+                lastname,
+                department,
+                img,
+                role
+            });
+        } catch {
+            console.error(Error);
+        }
+    }, [username, password, firstname, lastname, department, img, role])
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main className="w-full min-h-screen flex justify-center items-center">
+            <div className="w-48 flex flex-col justify-center items-center space-y-5"   >
+                <label className="input input-bordered flex items-center gap-2">
+                    <input
+                        type="text"
+                        className="grow"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input
+                        type="password"
+                        className="grow"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input
+                        type="text"
+                        className="grow"
+                        placeholder="Firstname"
+                        value={firstname}
+                        onChange={(e) => setFirstname(e.target.value)}
+                    />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input
+                        type="text"
+                        className="grow"
+                        placeholder="Lastname"
+                        value={lastname}
+                        onChange={(e) => setLastname(e.target.value)}
+                    />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input
+                        type="text"
+                        className="grow"
+                        placeholder="Department"
+                        value={department}
+                        onChange={(e) => setDepartment(e.target.value)}
+                    />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input
+                        type="text"
+                        className="grow"
+                        placeholder="Img"
+                        value={img}
+                        onChange={(e) => setImg(e.target.value)}
+                    />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input
+                        type="text"
+                        className="grow"
+                        placeholder="Role"
+                        value={role}
+                        onChange={(e) => setRole(e.target.value)}
+                    />
+                </label>
+                <CldUploadWidget
+                    onUpload={handleUpload}
+                    uploadPreset='n2sb3ooy'
+                    options={{
+                        maxFiles: 1
+                    }}
+                >
+                    {({ open }) => {
+                        return (
+                            <div
+                                onClick={() => open?.()}
+                            >
+                                <div className='font-semibold text-lg'>
+                                    Click to upload
+                                </div>
+                            </div>
+                        )
+                    }}
+                </CldUploadWidget>
+                {uploadedImageUrl && (
+                    <img src={uploadedImageUrl} alt="Uploaded" className="w-40 h-40 object-cover mt-4" />
+                )}
+                <button className="btn btn-accent" onClick={handleSubmit}>
+                    Log State
+                </button>
+            </div>
+        </main>
+    );
 }
+
+// #DATABASE_URL=""
+// NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME=""
+// https://next.cloudinary.dev/clduploadbutton/basic-usage
+// cloudinary
